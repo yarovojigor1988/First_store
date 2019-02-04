19f572a58e9e9a55cd1fc185dff40d06326df5f3
yarovojigor1988 <yarovojigor1988@gmail.com>
2019-02-04 20:20:30 +0200

    tabs

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+$(document).on('click', '.tabs_nav-link', function(e){
+    e.preventDefault();
+    var tabId = $(this).attr('href');
+    $(this).addClass('active');
+    $(this).parent('li').siblings().find('.tabs_nav-link').removeClass('active');
+    $(tabId).show(400);
+    $(tabId).siblings('.tabs_tab').hide(400);
+});
