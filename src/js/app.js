2e77f6d0c5e8f2c329a1643f654b48844687bc84
yarovojigor1988 <yarovojigor1988@gmail.com>
2019-02-09 10:30:25 +0200

    all changes

--- src/js/app.js
+++ src/js/app.js
@@ -9,7 +9,20 @@ $(document).on('click', '.tabs_nav-link', function(e){
 
 
 $('.product_carousel').slick({
-    dots: true
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    skidesToshow: 4
+
+    // responsive: [
+    //     {
+    //         breakpoint: 375,
+    //         settings: {
+    //             slidesToshow: 1
+    //         }
+    //     }
+    // ]
+
 });
 
 $('.slick-next.slick-next').slick({
@@ -18,4 +31,19 @@ $('.slick-next.slick-next').slick({
 
 $('.slick-prev.slick-arrow').slick({
     arrows:false
+});
+
+$(document).ready(function() {
+    $('.js_login-popup').magnificPopup({
+        items: {
+            src: '#popup',
+            type: 'inline'
+        }
+    });
+});
+
+$(document).on('click','.js_collapse', function(){
+    $(this).toggleClass('active');
+    $(this).next('.footer_col-main').slideToggle(400);
+    $(this).parrents('.flex_col').siblings().find('.footer_col-main').slideUp(400);
 });
