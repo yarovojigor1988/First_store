598e5cc7a7a5acbc2a470b20c250fd9207deacfa
Kirill Biezdielnyi <ghostkirill@gmail.com>
2019-02-11 21:55:52 +0200

    add new page

--- src/js/app.js
+++ src/js/app.js
@@ -7,13 +7,14 @@ $(document).on('click', '.tabs_nav-link', function(e){
     $(tabId).siblings('.tabs_tab').hide(400);
 });
 
-
+if($('.product_carousel').length > 0) {
 $('.product_carousel').slick({
     arrows: false,
     dots: true,
     infinite: true,
     autoplay: true,
     slidesToShow: 4,
+    arrows: false,
    
     responsive: [
         {
@@ -35,14 +36,7 @@ $('.product_carousel').slick({
 
 });
 
-
-$('.slick-next.slick-arrow').slick({
-    arrows: false
-});
-
-$('.slick-prev.slick-arrow').slick({
-    arrows: false
-});
+}
 
 $(document).ready(function() {
     $('.js_login-popup').magnificPopup({
@@ -57,4 +51,46 @@ $(document).on('click','.js_collapse', function(){
     $(this).toggleClass('active');
     $(this).next('.footer_col-main').slideToggle(400);
     $(this).parrents('.flex_col').siblings().find('.footer_col-main').slideUp(400);
+});
+
+
+$(document).ready(function() {
+if($('.js_login-popup').length > 0) {
+    $('.js_login-popup').magnificPopup({
+        items: {
+            src: '#popup',
+            type: 'inline'
+            }
+        });
+    }
+
+if($('.js_range').length > 0) {
+    $('.js_range').ionRangeSlider({
+            skin: "round",
+            type: "double",
+            min: 0,
+            max: 1000,
+            to: 600,
+            prefix: "$",
+            hide_min_max: true
+        });
+    }
+
+    if($('.js_products-table').length > 0){
+        $('.js_products-list').on('click', function(){
+            $(this).toggleClass('grid_active');
+            $(this).parent().siblings().child('.products_grid-btn').removeClass('.grid_active');
+            $('.products_grid').removeClass('js_grid-table');
+            $('.products_grid').addClass('js_grid-list');
+        })
+    }
+
+    if($('.js_products-list').length > 0){
+        $('.js_products-table').on('click', function(){
+            $(this).toggleClass('grid_active');
+            $(this).parent().siblings().child('.products_grid-btn').removeClass('.grid_active');
+            $('.products_grid').removeClass('js_grid-list');
+            $('.products_grid').addClass('js_grid-table');
+        })
+    }
 });
